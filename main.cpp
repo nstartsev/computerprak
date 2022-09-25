e941beb8792a1f573c80e205a11b448b2d5321a3
nstatsev <startsev.nikolay00@gmail.com>
2022-09-25 19:02:33 +0300

    "init"

--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <math.h>
+using namespace std;
 
 int main() {
-    double a = 1;
-    double b = 1;
-    double c;
-    c = a + b;
-    std::cout<< a+b << std::endl;
-    std::cout<< 'ok' ;
+    //mesh
+    double Xmax = 10;
+    int Im = 500;
+    int Imax = 1000;
+    vector <double> x(Imax,0);
+    double hx = 2*Xmax/(Im);
+    for (size_t i = 0; i <= Im; ++i){
+        x[i] = hx*i - Xmax;
+    }
+
+    // init u0(x)
+    vector <double> u0(Imax,0);
+    for (size_t i = 0; i <= Im; ++i){
+        if (abs(x[i]) <= 1){
+            u0[i] = 1-x[i]*x[i];
+        }
+    }
+
+    vector <double> u(Imax, 0);
+    for (size_t i = 0 ; i <= Im; ++i){
+        u[i] = u0[i];
+    }
+
+    double Tmax = 10;
+    double dt = 0.05;
+
 }
