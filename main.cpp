f9107829ec154d3783a100e955fbeb02316c2f7c
nstatsev <startsev.nikolay00@gmail.com>
2022-09-26 14:03:38 +0300

    "scalar"

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <vector>
-#include <math.h>
+#include <cmath>
 #include <fstream>
 
 using namespace std;
 
-int main() {
+int main(int argc, char **argv) {
+
     //mesh
     double Xmax = 20;
     int Im = 500;
@@ -17,12 +18,23 @@ int main() {
     }
 
     // init u0(x)
+    int initial_condition = 1;
     vector <double> u0(Imax,0);
-    for (size_t i = 0; i <= Im; ++i){
-        if (abs(x[i]) <= 1){
-            u0[i] = (1-abs(x[i]));
+
+    if (initial_condition == 0){
+        for (size_t i = 0; i <= Im; ++i){
+            if (abs(x[i]) <= 1){
+                u0[i] = (1-abs(x[i]));
+            }
         }
     }
+    if (initial_condition == 1){
+        for (size_t i = 0; i <= Im; ++i){
+                u0[i] = (1-abs(x[i])/Xmax);
+        }
+    }
+
+
 
     vector <double> u(Imax, 0);
     vector <double> u_new(Imax, 0);
@@ -34,25 +46,47 @@ int main() {
     double dt = 0.05;
     double t = 0;
     double a = 1.;
+    int problem = 1;
 
     ofstream sol;
     sol.open("solution.dat");
     sol << "TITLE= 'SOLUTION'" << endl;
     sol << "VARIABLES= " << "'X'" << '\t' << "'U'" << endl;
 
-    cout << hx << endl << a*dt << endl;
+    cout << "hx= " << hx << endl << "adt= " <<  abs(a*dt) << endl;
     // algorithm
-    while (t <= 10.0){
+    while (t <= Tmax){
         //print solution
         sol << "ZONE T= " << "'" << t << "'" << '\t' << "I= " << Im << '\t' << "F=POINT" << endl;
         sol << "SOLUTIONTIME= " << t << endl;
         for (size_t i = 0 ; i < Im; ++i){
             sol << x[i] << '\t' << u[i] << endl;
         }
-        for (size_t i = 1; i < Im; ++i){
-            u_new[i] = u[i] - dt*(a*(u[i]-u[i-1])/(hx));
+        if (problem == 0){
+            if (a>0){
+                for (size_t i = 1; i < Im; ++i){
+                    u_new[i] = u[i] - dt*(a*(u[i]-u[i-1])/(hx));
+                }
+            }
+            else{
+                for (size_t i = 1; i < Im; ++i){
+                    u_new[i] = u[i] - dt*(a*(u[i+1]-u[i])/(hx));
+                }
+            }
         }
-        for (size_t i = 1; i < Im; ++i){
+        if (problem == 1){
+            if (a>0){
+                for (size_t i = 1; i < Im; ++i){
+                    u_new[i] = u[i] - dt*(u[i]*(u[i]-u[i-1])/(hx));
+                }
+            }
+            else{
+                for (size_t i = 1; i < Im; ++i){
+                    u_new[i] = u[i] - dt*(u[i]*(u[i+1]-u[i])/(hx));
+                }
+            }
+        }
+        for (size_t i = 1; i < Im; ++i) {
             u[i] = u_new[i];
         }
         t += dt;
